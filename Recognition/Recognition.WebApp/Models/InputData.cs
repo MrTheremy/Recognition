ab572983a0cbbb45fb2df2576e2bb68f1cc1d072
Jolan Eysackers <Jolan.Eysackers@realcore.de>
2025-01-31 13:56:01 +0100

    Initial commit

---
+++ Recognition/Recognition.WebApp/Models/InputData.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.ML;
+using Microsoft.ML.Data
+
+
+namespace Recognition.WebApp.Models
+{
+    public class InputData
+    {
+        [ImageInput]
+        public Image<Bgr, byte> Image { get; set; }
+    }
+}
---
+++ Recognition/Recognition.WebApp/Models/OutputData.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.ML.Data;
+
+namespace Recognition.WebApp.Models
+{
+    public class OutputData
+    {
+        [ColumnName("PredictedLabel")]
+        public string Prediction { get; set; }
+    }
+}
